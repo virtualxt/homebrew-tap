2eb2225455491168e030079187a67d6eb5954027
Andreas T Jonsson <mail@andreasjonsson.se>
2020-09-02 14:18:26 +0200

    Update virtualxt.rb

--- virtualxt.rb
+++ virtualxt.rb
@@ -1,25 +1,30 @@
 class Virtualxt < Formula
-    desc "A portable, lightweight IBM PC/XT emulator written in Go."
-    homepage "http://virtualxt.org"
-    head "https://github.com/andreas-jonsson/virtualxt.git", :branch => "master"
-    url "https://github.com/andreas-jonsson/virtualxt/archive/v0.5.zip"
-    version "0.5.0"
-    sha256 "d1d7a2f125b83a9855858007b1c45081fd478074d93016a976edb6998bd3ea6c"
-    
-    depends_on "pkg-config" => :build
-    depends_on "go" => :build
-    depends_on "sdl2"
+  desc "VirtualXT is a portable, lightweight IBM PC/XT emulator written in Go"
+  homepage "https://virtualxt.org"
+  url "https://github.com/andreas-jonsson/virtualxt/archive/v0.5.zip"
+  head "https://github.com/andreas-jonsson/virtualxt.git", branch: "master"
+  sha256 "d1d7a2f125b83a9855858007b1c45081fd478074d93016a976edb6998bd3ea6c"
+  license "GPL-3.0-or-later"
+  version "0.5.0"
 
-    def install
-        system "FULL_VERSION=#{version}.0 go generate ./..."
-        system "go build -tags sdl"
+  depends_on "go" => [:build, :test]
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "sdl2"
 
-        bin.install "virtualxt"
-        bin.install "tools/package/homebrew/virtualxt.freedos"
-        
-        pkgshare.install "doc/manual" => "manual"
-        pkgshare.install "bios/pcxtbios.bin"
-        pkgshare.install "bios/vxtcga.bin"
-        pkgshare.install "boot/freedos.img"
-    end
+  def install
+    system "FULL_VERSION=#{version}.0", "go", "generate", "./..."
+    system "go", "build", "-tags", "sdl"
+
+    bin.install "virtualxt"
+    bin.install "tools/package/homebrew/virtualxt.freedos"
+
+    pkgshare.install "doc/manual" => "manual"
+    pkgshare.install "bios/pcxtbios.bin"
+    pkgshare.install "bios/vxtcga.bin"
+    pkgshare.install "boot/freedos.img"
+  end
+  
+  test do
+    system "go", "test", "./..."
+  end
 end
