f222c59feca114f28271cddd0d85f4f2afe4ed28
Andreas T Jonsson <mail@andreasjonsson.se>
2020-09-02 14:36:11 +0200

    Update virtualxt.rb

--- virtualxt.rb
+++ virtualxt.rb
@@ -2,8 +2,8 @@
 
 class Virtualxt < Formula
   desc "Portable, lightweight IBM PC/XT emulator written in Go"
-  head "https://github.com/andreas-jonsson/virtualxt.git", branch: "master"
   homepage "https://virtualxt.org"
+  head "https://github.com/andreas-jonsson/virtualxt.git", branch: "master"
   url "https://github.com/andreas-jonsson/virtualxt/archive/v0.5.zip"
   sha256 "d1d7a2f125b83a9855858007b1c45081fd478074d93016a976edb6998bd3ea6c"
   version "0.5.0"
