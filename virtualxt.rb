10e63e119efc9a95809690e19d1a0b443bced17d
Andreas T Jonsson <mail@andreasjonsson.se>
2020-09-02 14:39:36 +0200

    Update virtualxt.rb

--- virtualxt.rb
+++ virtualxt.rb
@@ -6,8 +6,8 @@ class Virtualxt < Formula
   url "https://github.com/andreas-jonsson/virtualxt/archive/v0.5.zip"
   version "0.5.0"
   sha256 "d1d7a2f125b83a9855858007b1c45081fd478074d93016a976edb6998bd3ea6c"
-  head "https://github.com/andreas-jonsson/virtualxt.git", branch: "master"
   license "GPL-3.0-or-later"
+  head "https://github.com/andreas-jonsson/virtualxt.git", branch: "master"
 
   depends_on "go" => [:build, :test]
   depends_on "pkg-config" => [:build, :test]
