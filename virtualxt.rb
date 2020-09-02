37d93522de3ec287b5b35692ef1b63095028b8b9
Andreas T Jonsson <mail@andreasjonsson.se>
2020-09-02 07:58:07 +0000

    Update virtualxt.rb

--- virtualxt.rb
+++ virtualxt.rb
@@ -3,7 +3,7 @@ class Virtualxt < Formula
     homepage "http://virtualxt.org"
     url "https://github.com/andreas-jonsson/virtualxt/archive/v0.5.zip"
     version "0.5.0"
-    sha256 "D1D7A2F125B83A9855858007B1C45081FD478074D93016A976EDB6998BD3EA6C"
+    sha256 "d1d7a2f125b83a9855858007b1c45081fd478074d93016a976edb6998bd3ea6c"
 
     depends_on "pkg-config" => :build
     depends_on "go" => :build
