05c067c3006955f71c6372e61c4a5512ceb373ac
Andreas T Jonsson <mail@andreasjonsson.se>
2022-12-05 11:50:34 +0100

    Updated to v0.7

--- virtualxt.rb
+++ virtualxt.rb
@@ -1,28 +1,25 @@
 # frozen_string_literal: true
 
 class Virtualxt < Formula
-  desc "Portable, lightweight IBM PC/XT emulator written in Go"
+  desc "Portable, lightweight IBM PC/XT emulator written in C"
   homepage "https://virtualxt.org"
-  url "https://github.com/andreas-jonsson/virtualxt/archive/v0.6.1.tar.gz"
-  sha256 "6619e16925ab488bc7ee09852d05c98f758050aef9c115eec70a283c9bdc04dc"
-  license "Zlib"
+  url "https://github.com/andreas-jonsson/virtualxt/archive/v0.7.0.tar.gz"
+  sha256 "3077257ecd643749e29f9a7d3d1418fbeacc23f422b83bd398ebb8e5adf3a4f4"
+  license "zlib-acknowledgement"
   head "https://github.com/andreas-jonsson/virtualxt.git", branch: "edge"
 
-  depends_on "go" => :build
+  depends_on "zig" => :build
   depends_on "pkg-config" => :build
   depends_on "sdl2"
 
   def install
-    ENV["FULL_VERSION"] = "#{version}.0"
+    system "zig", "build", "-Drelease-fast", "-Dv20"
 
-    system "go", "generate", "./..."
-    system "go", "build", "-tags", "sdl,pcap"
-
-    bin.install "virtualxt"
+    bin.install "build/bin/virtualxt"
     bin.install "tools/package/homebrew/virtualxt.freedos"
 
-    pkgshare.install "doc/manual" => "manual"
     pkgshare.install "bios/pcxtbios.bin"
+    pkgshare.install "bios/glabios.bin"
     pkgshare.install "bios/vxtx.bin"
     pkgshare.install "boot/freedos_hd.img"
   end
