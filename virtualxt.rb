22b4d2f21232fc10200092f4d006cc17c324fe34
Andreas T Jonsson <mail@andreasjonsson.se>
2024-06-20 14:39:39 +0200

    Updated to v1.0

--- virtualxt.rb
+++ virtualxt.rb
@@ -1,26 +1,30 @@
 # frozen_string_literal: true
 
 class Virtualxt < Formula
-  desc "Portable, lightweight IBM PC/XT emulator written in C"
+  desc "Portable, lightweight PC/XT emulator written in C"
   homepage "https://virtualxt.org"
-  url "https://github.com/andreas-jonsson/virtualxt/archive/v0.7.0.tar.gz"
-  sha256 "3077257ecd643749e29f9a7d3d1418fbeacc23f422b83bd398ebb8e5adf3a4f4"
+  url "https://github.com/andreas-jonsson/virtualxt/archive/v1.0.tar.gz"
+  sha256 "b5662318abee4caf20e5684ad82f2b7cf38c98a3e8889127eac4d6af03e79644"
   license "zlib-acknowledgement"
   head "https://github.com/andreas-jonsson/virtualxt.git", branch: "edge"
 
-  depends_on "zig" => :build
-  depends_on "pkg-config" => :build
+  depends_on "premake" => :build
+  depends_on "make" => :build
   depends_on "sdl2"
+  depends_on "libpcap"
 
   def install
-    system "zig", "build", "-Drelease-fast", "-Dv20"
+    system "premake5", "--modules", "--static", "gmake"
+    system "make", "sdl2-frontend"
 
     bin.install "build/bin/virtualxt"
     bin.install "tools/package/homebrew/virtualxt.freedos"
 
+    pkgshare.install "bios/GLABIOS.ROM"
+    pkgshare.install "bios/GLABIOS640.ROM"
+    pkgshare.install "bios/GLaTICK_0.8.4_AT.ROM"
     pkgshare.install "bios/pcxtbios.bin"
-    pkgshare.install "bios/glabios.bin"
-    pkgshare.install "bios/vxtx.bin"
+    pkgshare.install "boot/freedos.img"
     pkgshare.install "boot/freedos_hd.img"
   end
 
